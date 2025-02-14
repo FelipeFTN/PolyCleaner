ba4fefe16e78279c2824d1013f46427fcd84450e
FelipeFTN <FelipeFTN@protonmail.com>
2025-02-14 15:39:41 -0300

    allocating memory & replace string

---
+++ src/libs.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub const String = struct {
+    data: []const u8,
+    allocator: std.mem.Allocator,
+
+    pub fn init(s: []const u8, allocator: std.mem.Allocator) !String {
+        const data = try allocator.dupe(u8, s); // Proper allocation
+        return .{ .data = data, .allocator = allocator };
+    }
+
+    pub fn contains(self: String, substr: []const u8) bool {
+        return std.mem.indexOf(u8, self.data, substr) != null;
+    }
+
+    pub fn replace(self: String, old: []const u8, new: []const u8) !String {
+        const index = std.mem.indexOf(u8, self.data, old) orelse return self;
+
+        // Calculate new size
+        const new_len: usize = @intCast(@as(usize, self.data.len) + @as(usize, new.len) - @as(usize, old.len));
+
+        // Allocate new memory
+        const new_data = try self.allocator.alloc(u8, new_len);
+
+        // Copy parts into new buffer
+        std.mem.copyForwards(u8, new_data[0..index], self.data[0..index]); // Copy before `old`
+        std.mem.copyForwards(u8, new_data[index .. index + new.len], new); // Copy `new`
+        std.mem.copyForwards(u8, new_data[index + new.len ..], self.data[index + old.len ..]); // Copy after `old`
+
+        return .{ .data = new_data, .allocator = self.allocator };
+    }
+
+    pub fn deinit(self: String) void {
+        self.allocator.free(self.data);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,28 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 const consts = @import("constants.zig");
+const libs = @import("libs.zig");
 
-fn get_cache_directories(allocator: std.mem.Allocator) !std.ArrayList([]const u8) {
-    var cache_directories = std.ArrayList([]const u8).init(allocator);
+fn get_cache_directories(allocator: std.mem.Allocator) !std.ArrayList(libs.String) {
+    var cache_directories = std.ArrayList(libs.String).init(allocator);
+
+    // Try to get the HOME environment variable, if it doesn't exist (null), use the default value.
+    const HOME = std.posix.getenv("HOME") orelse "~";
+    std.debug.print("HOME: {s}\n", .{HOME});
 
     if (builtin.target.isGnu()) {
         for (consts.LINUX_CACHE_DIRS) |dir| {
-            try cache_directories.append(dir);
+            var directory = try libs.String.init(dir, allocator);
+            directory = try directory.replace("$HOME", HOME);
+
+            try cache_directories.append(directory);
         }
     } else if (builtin.target.isDarwin()) {
         for (consts.MAC_OS_CACHE_DIRS) |dir| {
-            try cache_directories.append(dir);
+            var directory = try libs.String.init(dir, allocator);
+            directory = try directory.replace("$HOME", HOME);
+
+            try cache_directories.append(directory);
         }
     }
 
@@ -27,6 +38,6 @@ pub fn main() !void {
     defer cache_directories.deinit();
 
     for (cache_directories.items) |dir| {
-        std.debug.print("Cache directory: {s}\n", .{dir});
+        std.debug.print("Cache directory: {s}\n", .{dir.data});
     }
 }
