abb2c9850805787011eb3873a5490d8243c382a8
FelipeFTN <FelipeFTN@protonmail.com>
2025-02-14 18:57:40 -0300

    finally removing all cache files & recreating directories

--- src/main.zig
+++ src/main.zig
@@ -30,6 +30,38 @@ fn get_cache_directories(allocator: std.mem.Allocator) !std.ArrayList(libs.Strin
     return cache_directories;
 }
 
+fn delete_cache_directories(cache_dirs: std.ArrayList(libs.String)) std.fs.Dir.DeleteDirError!void {
+    if (cache_dirs.items.len == 0) {
+        std.debug.print("Error creating cache directories: cache_dirs is null\n", .{});
+        return;
+    }
+
+    for (cache_dirs.items) |dir| {
+        const err = std.fs.cwd().deleteTree(dir.data);
+        if (@TypeOf(err) == std.fs.Dir.DeleteDirError) {
+            std.debug.print("Error deleting cache directory: {s}\n", .{dir.data});
+        }
+
+        std.debug.print("Deleted cache directory: {s}\n", .{dir.data});
+    }
+}
+
+fn create_cache_directories(cache_dirs: std.ArrayList(libs.String)) std.fs.Dir.MakeError!void {
+    if (cache_dirs.items.len == 0) {
+        std.debug.print("Error creating cache directories: cache_dirs is null\n", .{});
+        return;
+    }
+
+    for (cache_dirs.items) |dir| {
+        const err = std.fs.cwd().makeDir(dir.data);
+        if (@TypeOf(err) == std.fs.Dir.MakeError) {
+            std.debug.print("Error creating cache directory: {s}\n", .{dir.data});
+        }
+
+        std.debug.print("Created cache directory: {s}\n", .{dir.data});
+    }
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -37,7 +69,6 @@ pub fn main() !void {
     var cache_directories = try get_cache_directories(allocator);
     defer cache_directories.deinit();
 
-    for (cache_directories.items) |dir| {
-        std.debug.print("Cache directory: {s}\n", .{dir.data});
-    }
+    try delete_cache_directories(cache_directories);
+    try create_cache_directories(cache_directories);
 }
