20503501a773131aecb4f9284983dccdaa11e4b5
FelipeFTN <FelipeFTN@protonmail.com>
2025-02-14 19:21:16 -0300

    simplifying cache & freeing up memory resources

---
+++ src/cache.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const consts = @import("constants.zig");
+const libs = @import("libs.zig");
+
+pub fn get_cache_directories(allocator: std.mem.Allocator) !std.ArrayList(libs.String) {
+    var cache_directories = std.ArrayList(libs.String).init(allocator);
+
+    // Try to get the HOME environment variable, if it doesn't exist (null), use the default value.
+    const HOME = std.posix.getenv("HOME") orelse "~";
+    std.debug.print("HOME: {s}\n", .{HOME});
+
+    if (builtin.target.isGnu()) {
+        for (consts.LINUX_CACHE_DIRS) |dir| {
+            var directory = try libs.String.init(dir, allocator);
+            directory = try directory.replace("$HOME", HOME);
+
+            try cache_directories.append(directory);
+        }
+    } else if (builtin.target.isDarwin()) {
+        for (consts.MAC_OS_CACHE_DIRS) |dir| {
+            var directory = try libs.String.init(dir, allocator);
+            directory = try directory.replace("$HOME", HOME);
+
+            try cache_directories.append(directory);
+        }
+    }
+
+    return cache_directories;
+}
+
+pub fn delete_cache_directories(cache_dirs: std.ArrayList(libs.String)) std.fs.Dir.DeleteDirError!void {
+    if (cache_dirs.items.len == 0) {
+        std.debug.print("Error creating cache directories: cache_dirs is null\n", .{});
+        return;
+    }
+
+    for (cache_dirs.items) |dir| {
+        const err = std.fs.cwd().deleteTree(dir.data);
+        if (@TypeOf(err) == std.fs.Dir.DeleteDirError) {
+            std.debug.print("Error deleting cache directory: {s}\n", .{dir.data});
+        }
+
+        std.debug.print("Deleted cache directory: {s}\n", .{dir.data});
+    }
+}
+
+pub fn create_cache_directories(cache_dirs: std.ArrayList(libs.String)) std.fs.Dir.MakeError!void {
+    if (cache_dirs.items.len == 0) {
+        std.debug.print("Error creating cache directories: cache_dirs is null\n", .{});
+        return;
+    }
+
+    for (cache_dirs.items) |dir| {
+        const err = std.fs.cwd().makeDir(dir.data);
+        if (@TypeOf(err) == std.fs.Dir.MakeError) {
+            std.debug.print("Error creating cache directory: {s}\n", .{dir.data});
+        }
+
+        std.debug.print("Created cache directory: {s}\n", .{dir.data});
+    }
+}
+
+pub fn free_memory_resources(cache_dirs: std.ArrayList(libs.String)) void {
+    for (cache_dirs.items) |dir| {
+        dir.deinit();
+    }
+
+    cache_dirs.deinit();
+}
--- src/main.zig
+++ src/main.zig
@@ -1,74 +1,14 @@
 const std = @import("std");
-const builtin = @import("builtin");
 
-const consts = @import("constants.zig");
-const libs = @import("libs.zig");
-
-fn get_cache_directories(allocator: std.mem.Allocator) !std.ArrayList(libs.String) {
-    var cache_directories = std.ArrayList(libs.String).init(allocator);
-
-    // Try to get the HOME environment variable, if it doesn't exist (null), use the default value.
-    const HOME = std.posix.getenv("HOME") orelse "~";
-    std.debug.print("HOME: {s}\n", .{HOME});
-
-    if (builtin.target.isGnu()) {
-        for (consts.LINUX_CACHE_DIRS) |dir| {
-            var directory = try libs.String.init(dir, allocator);
-            directory = try directory.replace("$HOME", HOME);
-
-            try cache_directories.append(directory);
-        }
-    } else if (builtin.target.isDarwin()) {
-        for (consts.MAC_OS_CACHE_DIRS) |dir| {
-            var directory = try libs.String.init(dir, allocator);
-            directory = try directory.replace("$HOME", HOME);
-
-            try cache_directories.append(directory);
-        }
-    }
-
-    return cache_directories;
-}
-
-fn delete_cache_directories(cache_dirs: std.ArrayList(libs.String)) std.fs.Dir.DeleteDirError!void {
-    if (cache_dirs.items.len == 0) {
-        std.debug.print("Error creating cache directories: cache_dirs is null\n", .{});
-        return;
-    }
-
-    for (cache_dirs.items) |dir| {
-        const err = std.fs.cwd().deleteTree(dir.data);
-        if (@TypeOf(err) == std.fs.Dir.DeleteDirError) {
-            std.debug.print("Error deleting cache directory: {s}\n", .{dir.data});
-        }
-
-        std.debug.print("Deleted cache directory: {s}\n", .{dir.data});
-    }
-}
-
-fn create_cache_directories(cache_dirs: std.ArrayList(libs.String)) std.fs.Dir.MakeError!void {
-    if (cache_dirs.items.len == 0) {
-        std.debug.print("Error creating cache directories: cache_dirs is null\n", .{});
-        return;
-    }
-
-    for (cache_dirs.items) |dir| {
-        const err = std.fs.cwd().makeDir(dir.data);
-        if (@TypeOf(err) == std.fs.Dir.MakeError) {
-            std.debug.print("Error creating cache directory: {s}\n", .{dir.data});
-        }
-
-        std.debug.print("Created cache directory: {s}\n", .{dir.data});
-    }
-}
+const cache = @import("cache.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var cache_directories = try get_cache_directories(allocator);
-    defer cache_directories.deinit();
+    const cache_directories = try cache.get_cache_directories(allocator);
+    defer cache.free_memory_resources(cache_directories);
 
-    try delete_cache_directories(cache_directories);
-    try create_cache_directories(cache_directories);
+    try cache.delete_cache_directories(cache_directories);
+    try cache.create_cache_directories(cache_directories);
 }
